218f3429b4f66f8f36d204e8c540b09665d2c00e
wer032001 <wer032001@gmail.com>
2023-01-11 13:01:46 +0300

    JavaSyntax task0206 completed

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -3,7 +3,10 @@ package com.javarush.task.pro.task02.task0206;
 public class Solution {
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        String word = "Word";
+        String phrase = "Phrase";
+        String line = "line";
+        String text = "Text";
 
     }
 }
