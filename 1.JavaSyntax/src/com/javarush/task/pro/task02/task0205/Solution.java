7308f7fc30794fbafaa8c9e2150195844e9ea9b3
wer032001 <wer032001@gmail.com>
2023-01-11 12:56:33 +0300

    JavaSyntax task0203 completed

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0205/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0205/Solution.java
@@ -9,7 +9,15 @@ public class Solution {
     public static void main(String[] args) {
         int toothCounter = 32;
 
-
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
 
         System.out.println(toothCounter);
     }
