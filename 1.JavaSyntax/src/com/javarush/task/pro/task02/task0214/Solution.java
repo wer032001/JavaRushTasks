50c8e31aad48f37469a06a309e8741a2671219b5
wer032001 <wer032001@gmail.com>
2023-01-11 18:57:22 +0300

    JavaSyntax task0214 completed

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0214/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0214/Solution.java
@@ -10,6 +10,15 @@ public class Solution {
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+
+        String str1 = scanner.nextLine();
+        String str2 = scanner.nextLine();
+        String str3 = scanner.nextLine();
+
+        System.out.println(str3);
+        System.out.println(str2.toUpperCase());
+        System.out.println(str1.toLowerCase());
 
     }
 }
