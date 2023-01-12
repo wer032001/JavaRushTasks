97a8c2b846e8bfc2f73f5071e60bccf077953303
wer032001 <wer032001@gmail.com>
2023-01-12 18:30:17 +0300

    JavaSyntax task0305 completed

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0305/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0305/Solution.java
@@ -8,6 +8,13 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        if (a == b && b == c) System.out.println("" + a + " " + a + " " + a);
+        else if (a == b || b == c ) System.out.println("" + b + " " + b);
+        else if (a == c) System.out.println("" + a + " " + a);;
     }
 }
