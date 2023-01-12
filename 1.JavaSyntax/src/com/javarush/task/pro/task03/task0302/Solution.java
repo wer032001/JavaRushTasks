9fcc3595c652cb25d93127302e9c06a8933ceb2f
wer032001 <wer032001@gmail.com>
2023-01-12 13:44:26 +0300

    JavaSyntax task0302 completed

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
@@ -9,6 +9,12 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         String militaryCommissar = ", явитесь в военкомат";
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int age = scanner.nextInt();
+
+        if (age >= 18 && age <= 28) {
+            System.out.println(name + militaryCommissar);
+        }
     }
 }
