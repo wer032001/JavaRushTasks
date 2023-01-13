cb56cb32aafe5cdbcaf3fb3bf111e32648b6eff4
wer032001 <wer032001@gmail.com>
2023-01-13 22:17:59 +0300

    task0308 completed

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0308/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0308/Solution.java
@@ -8,6 +8,13 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        if ((x > 0) && (y > 0)) System.out.println("1");
+        if ((x < 0) && (y > 0)) System.out.println("2");
+        if ((x < 0) && (y < 0)) System.out.println("3");
+        if ((x > 0) && (y < 0)) System.out.println("4");
     }
 }
