9419a23eebf2627d4b62cb9088f092c1f9618974
wer032001 <wer032001@gmail.com>
2023-01-14 13:35:34 +0300

    task0310 completed

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0310/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0310/Solution.java
@@ -10,7 +10,12 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        String result = number < 5 ? "число меньше 5" : number > 5 ? "число больше 5" : "число равно 5";
-        System.out.println(result);
+        if (number == 5) {
+            System.out.println("число равно 5");
+        } else if (number > 5) {
+            System.out.println("число больше 5");
+        } else {
+            System.out.println("число меньше 5");
+        }
     }
 }
