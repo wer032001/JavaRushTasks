20762751894aca496046550bb02d5561f816b529
wer032001 <wer032001@gmail.com>
2023-01-14 20:19:48 +0300

    task0402 completed

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0402/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0402/Solution.java
@@ -9,7 +9,12 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         String text = " любит меня.";
-        //напишите тут ваш код
-
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int n = 0;
+        while (n < 10) {
+            System.out.println(name + text);
+            n++;
+        }
     }
 }
