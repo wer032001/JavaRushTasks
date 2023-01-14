187e030bc7b884268e4264cbde9efae9737a83ed
wer032001 <wer032001@gmail.com>
2023-01-14 22:17:51 +0300

    task0405 completed

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
@@ -6,7 +6,25 @@ package com.javarush.task.pro.task04.task0405;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        int n = 0;
+        while (n < 10) {
+            if ((n == 0) || (n == 9)) {
+                int m = 0;
+                while (m < 20) {
+                    System.out.print("Б");
+                    m++;
+                }
+            } else {
+                System.out.print("Б");
+                int m = 0;
+                while (m < 18) {
+                    System.out.print(" ");
+                    m++;
+                }
+                System.out.print("Б");
+            }
+            System.out.println();
+            n++;
+        }
     }
 }
