d1af49a82faa25d1add65ba81df5ef4006d712b9
wer032001 <wer032001@gmail.com>
2023-01-14 21:53:52 +0300

    task0403 completed

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
@@ -8,7 +8,15 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        while (true) {
+            if (scanner.hasNextInt()) {
+                sum = sum + scanner.nextInt();
+            }
+            if (scanner.nextLine().equals("ENTER"))
+                break;
+        }
+        System.out.println(sum);
     }
 }
