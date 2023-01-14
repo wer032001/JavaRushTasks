5e2d60abf5fe58de60144f1ac2cd5bf295c106d7
wer032001 <wer032001@gmail.com>
2023-01-14 23:51:49 +0300

    task0407 completed

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0407/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0407/Solution.java
@@ -6,7 +6,14 @@ package com.javarush.task.pro.task04.task0407;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
-    }
+        int sum = 0;
+        int n = 0;
+        while (n < 100) {
+            n++;
+            if ((n % 3) == 0)
+                continue;
+                sum = sum + n ;
+            }
+        System.out.println(sum);
+        }
 }
