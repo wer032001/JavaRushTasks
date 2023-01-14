f1d6662caa54271a4775656ee43ba9e0483aad7c
wer032001 <wer032001@gmail.com>
2023-01-14 21:59:42 +0300

    task0404 completed

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
@@ -6,7 +6,15 @@ package com.javarush.task.pro.task04.task0404;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        int n = 0;
+        while (n < 5) {
+            int m = 0;
+            while (m < 10) {
+                System.out.print("Q");
+                m++;
+            }
+            System.out.println();
+            n++;
+        }
     }
 }
