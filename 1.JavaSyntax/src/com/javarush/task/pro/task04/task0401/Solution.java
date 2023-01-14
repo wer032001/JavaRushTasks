5a88990a1c97b3c544adf25f6dce604cc8f4acfa
wer032001 <wer032001@gmail.com>
2023-01-14 20:13:57 +0300

    task0401 completed

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -7,7 +7,10 @@ package com.javarush.task.pro.task04.task0401;
 public class Solution {
     public static void main(String[] args) {
         String quote = "Я никогда не буду работать за копейки. Амиго";
-        //напишите тут ваш код
-
+        int n = 0;
+        while (n < 100) {
+            System.out.println(quote);
+            n++;
+        }
     }
 }
