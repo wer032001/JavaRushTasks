d0d27d12acae25ec0721129ea5f9387a0b411193
wer032001 <wer032001@gmail.com>
2023-01-15 14:51:20 +0300

    task0416 completed

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0416/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0416/Solution.java
@@ -8,7 +8,11 @@ Share a Coke
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int jar = scanner.nextInt();
+        int people = scanner.nextInt();
+
+        System.out.println((jar * 1.0) / people);
 
     }
 }
