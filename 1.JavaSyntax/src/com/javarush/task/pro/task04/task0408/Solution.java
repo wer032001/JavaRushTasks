3ec6d1febf620eb6e73870c2c621be0ed8b0564d
wer032001 <wer032001@gmail.com>
2023-01-15 12:47:12 +0300

    task0408 completed

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0408/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0408/Solution.java
@@ -8,7 +8,14 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        Scanner scanner = new Scanner(System.in);
+        int max = Integer.MIN_VALUE;
+        while (scanner.hasNextInt()) {
+            int x = scanner.nextInt();
+            if (((x > max) && ((x % 2) == 0))) {
+                max = x;
+            }
+        }
+        System.out.println(max);
     }
 }
