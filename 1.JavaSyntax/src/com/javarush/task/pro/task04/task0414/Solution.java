9d2745a659cf69fecdd2b64d47d9b48427359923
wer032001 <wer032001@gmail.com>
2023-01-15 13:36:06 +0300

    task0414 completed

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0414/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0414/Solution.java
@@ -8,7 +8,15 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        int num = scanner.nextInt();
+        int n = 0;
+        do {
+            System.out.println(str);
+            if ((num <= 0) || ( num >= 5))
+                break;
+            n++;
+        } while (n < num);
     }
 }
