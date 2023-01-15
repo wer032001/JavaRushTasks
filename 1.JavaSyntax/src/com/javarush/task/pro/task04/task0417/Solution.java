c0d17cde69d4a78e0ba60c92a4589756a2e80248
wer032001 <wer032001@gmail.com>
2023-01-15 15:42:08 +0300

    task0417 completed

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0417/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task04.task0417;
+
+import java.util.Scanner;
+
+/* 
+Скорость ветра
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int s = scanner.nextInt();
+        System.out.println(Math.round(s * 3.6));
+    }
+}
