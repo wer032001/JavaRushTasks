2e60708caf988877c005a04af1936b38a91fde58
wer032001 <wer032001@gmail.com>
2023-01-15 14:44:47 +0300

    task0415 completed

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0415/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0415/Solution.java
@@ -8,7 +8,9 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        Scanner scanner = new Scanner(System.in);
+        int radius = scanner.nextInt();
+        double pi = 3.14;
+        System.out.println((int) (pi * radius * radius));
     }
 }
